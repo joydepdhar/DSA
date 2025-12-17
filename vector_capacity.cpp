ce4aeca75b0793c11dc3c0b93d592a39b1088cb8
joydepdhar <joydepdhar138@gmail.com>
2025-12-17 22:28:18 +0600

    stl

---
+++ vector_capacity.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int>v;
+    cout<<v.capacity()<<endl;
+    return 0;
+}
---
+++ vector_initialization.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // vector<int> v; //type 1
+    // cout<<v.size()<< endl;
+    // vector<int>v(10); type 2
+    // cout<<v.size()<<endl;
+    // vector<int> v(10,-1); type 3
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i]<<endl;
+    // }
+    // int a[5]={1,2,3,4,5};
+    // vector<int> v(a,a+5);
+    // for(int i=0;i<v.size();i++){
+    //     cout<<v[i]<<endl;
+    // }
+    vector<int> v={1,2,3,4,5};
+    for(int i;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+    return 0;
+}
