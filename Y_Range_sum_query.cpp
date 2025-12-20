8e5b19bf854f2a2aebab06b3951641a46b194b53
joydepdhar <joydepdhar138@gmail.com>
2025-12-20 21:59:48 +0600

    problem

---
+++ Y_Range_sum_query.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n,x;
+    cin>>n>>x;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    while (x--)
+    {
+        int a,b,sum=0;
+        cin>>a>>b;
+        for(int i=1;i<n;i++){
+        if(i>=a && i<=b){
+            sum+=arr[i];
+        }
+    }
+        // cout<<a<<" "<<b;
+        cout<<sum<<endl;
+    }
+    
+    return 0;
+}
